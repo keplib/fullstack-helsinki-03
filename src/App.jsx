56ce276facc596c4a08a22aa7fe5502df88f4f0e
keplib <keplib@gmail.com>
2022-11-17 22:24:56 +0100

    Part2 : 2.6 solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import './App.css'
 
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const submitName = (event) => {
+    event.preventDefault();
+    let copy = [...persons]
+    copy.push({ name: newName })
+    setPersons(copy);
+    console.log(persons)
+    console.log(copy)
+  }
+
+  const inputChangeHandler = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+    console.log(newName)
+  }
 
   return (
     <div>
-      <p>Hello World!</p>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitName}>
+        <div>
+          name: <input value={newName} placeholder='insert a name here' onChange={inputChangeHandler} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
