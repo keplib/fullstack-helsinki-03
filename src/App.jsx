c49b7efe556260ba83e169c456dc0aa5a4d3ab6f
keplib <keplib@gmail.com>
2022-11-21 15:17:24 +0100

    Part2: 2.9 solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: 123456 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
@@ -62,7 +66,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      <p>value of filter is: {filter}</p>
+      {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+        .map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
