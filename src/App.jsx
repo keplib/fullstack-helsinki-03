baecf9c28989c5f430b9e134e374037838678b45
keplib <keplib@gmail.com>
2022-11-24 11:06:43 +0100

    Part2: 2.11. solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import './App.css'
 
+import axios from 'axios';
+
 import Filter from './components/Filter'
 import AddNew from './components/AddNew'
 import ContactList from './components/ContactList'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, []);
 
   return (
     <div>
