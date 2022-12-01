34133c72590e26c2d3759998a968a5794db45614
keplib <keplib@gmail.com>
2022-12-01 11:57:28 +0100

    Part2: 2.17 delete server side implemented

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,8 +1,23 @@
+import personService from '../services/person.js';
+
 const ContactList = ({ persons, filter }) => {
+
+    const deleteHandler = (per) => {
+        console.log(personService.baseUrl + per.id);
+        personService
+            .deletePerson(personService.baseUrl + per.id)
+            .then(response => {
+                console.log(response.data);
+            });
+    }
+
     return (
         <div>
             {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-                .map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+                .map(person =>
+                    <p key={person.name}>
+                        {person.name} {person.number} <button onClick={() => deleteHandler(person)}>Delete</button>
+                    </p>)}
         </div>
     )
 }
--- src/services/person.js
+++ src/services/person.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3000/persons';
+const baseUrl = 'http://localhost:3000/persons/';
 
 const getAll = () => axios.get(baseUrl);
 const createNew = newPerson => axios.post(baseUrl, newPerson);
+const deletePerson = sourceUrl => axios.delete(sourceUrl);
 
-export default { getAll, createNew };
+export default { getAll, createNew, deletePerson, baseUrl };
